4d37996073333042e226d9992f3f2e0343364ff2
Emirhan Kaaner <kaaner4mir@gmail.com>
2025-11-23 23:17:02 +0300

    feat(ui): implement custom title bar controls and drag logic
    - Added user32.dll imports for window dragging (SendMessage).
    - Implemented close (Application.Exit), minimize, and toggle maximize actions.
    - Attached events to custom buttons.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,57 +29,97 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.pnlWindow = new System.Windows.Forms.Panel();
+            this.btnWindowControl = new System.Windows.Forms.Button();
+            this.btnIconStatus = new System.Windows.Forms.Button();
+            this.btnCrossMark = new System.Windows.Forms.Button();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.panel1.SuspendLayout();
+            this.pnlWindow.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // panel1
+            // pnlWindow
             // 
-            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(64)))));
-            this.panel1.Controls.Add(this.pictureBox2);
-            this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1113, 41);
-            this.panel1.TabIndex = 0;
-            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.pnlWindow.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(215)))), ((int)(((byte)(215)))), ((int)(((byte)(215)))));
+            this.pnlWindow.Controls.Add(this.btnWindowControl);
+            this.pnlWindow.Controls.Add(this.btnIconStatus);
+            this.pnlWindow.Controls.Add(this.btnCrossMark);
+            this.pnlWindow.Controls.Add(this.pictureBox2);
+            this.pnlWindow.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlWindow.Location = new System.Drawing.Point(0, 0);
+            this.pnlWindow.Name = "pnlWindow";
+            this.pnlWindow.Size = new System.Drawing.Size(1250, 40);
+            this.pnlWindow.TabIndex = 0;
+            this.pnlWindow.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnlWindow_MouseDown);
+            // 
+            // btnWindowControl
+            // 
+            this.btnWindowControl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnWindowControl.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnWindowControl.BackgroundImage")));
+            this.btnWindowControl.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnWindowControl.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnWindowControl.FlatAppearance.BorderSize = 0;
+            this.btnWindowControl.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
+            this.btnWindowControl.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnWindowControl.Location = new System.Drawing.Point(1170, 0);
+            this.btnWindowControl.Name = "btnWindowControl";
+            this.btnWindowControl.Size = new System.Drawing.Size(40, 40);
+            this.btnWindowControl.TabIndex = 6;
+            this.btnWindowControl.UseVisualStyleBackColor = true;
+            this.btnWindowControl.Click += new System.EventHandler(this.btnWindowControl_Click);
+            // 
+            // btnIconStatus
+            // 
+            this.btnIconStatus.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnIconStatus.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnIconStatus.BackgroundImage")));
+            this.btnIconStatus.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnIconStatus.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnIconStatus.FlatAppearance.BorderSize = 0;
+            this.btnIconStatus.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
+            this.btnIconStatus.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnIconStatus.Location = new System.Drawing.Point(1130, 0);
+            this.btnIconStatus.Name = "btnIconStatus";
+            this.btnIconStatus.Size = new System.Drawing.Size(40, 40);
+            this.btnIconStatus.TabIndex = 5;
+            this.btnIconStatus.UseVisualStyleBackColor = true;
+            this.btnIconStatus.Click += new System.EventHandler(this.btnIconStatus_Click);
+            // 
+            // btnCrossMark
+            // 
+            this.btnCrossMark.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCrossMark.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnCrossMark.BackgroundImage")));
+            this.btnCrossMark.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnCrossMark.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCrossMark.FlatAppearance.BorderSize = 0;
+            this.btnCrossMark.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.btnCrossMark.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCrossMark.Location = new System.Drawing.Point(1210, 0);
+            this.btnCrossMark.Name = "btnCrossMark";
+            this.btnCrossMark.Size = new System.Drawing.Size(40, 40);
+            this.btnCrossMark.TabIndex = 4;
+            this.btnCrossMark.UseVisualStyleBackColor = true;
+            this.btnCrossMark.Click += new System.EventHandler(this.btnCrossMark_Click);
             // 
             // pictureBox2
             // 
             this.pictureBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(1049, 7);
+            this.pictureBox2.Location = new System.Drawing.Point(1127, 7);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(25, 25);
+            this.pictureBox2.Size = new System.Drawing.Size(0, 0);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 2;
             this.pictureBox2.TabStop = false;
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(1080, 7);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(25, 25);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
-            // 
             // panel2
             // 
-            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(27)))), ((int)(((byte)(183)))));
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(63)))), ((int)(((byte)(79)))));
             this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel2.Location = new System.Drawing.Point(0, 41);
+            this.panel2.Location = new System.Drawing.Point(0, 40);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(295, 614);
+            this.panel2.Size = new System.Drawing.Size(295, 652);
             this.panel2.TabIndex = 1;
             // 
             // Form1
@@ -87,28 +127,28 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(70)))), ((int)(((byte)(255)))));
-            this.ClientSize = new System.Drawing.Size(1113, 655);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(125)))), ((int)(((byte)(155)))));
+            this.ClientSize = new System.Drawing.Size(1250, 692);
             this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pnlWindow);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.panel1.ResumeLayout(false);
+            this.pnlWindow.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel pnlWindow;
         private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnCrossMark;
+        private System.Windows.Forms.Button btnIconStatus;
+        private System.Windows.Forms.Button btnWindowControl;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,7 +12,6 @@ namespace EdiWare
 {
     public partial class Form1 : Form
     {
-
         public const int WM_NCLBUTTONDOWN = 0xA1;
         public const int HT_CAPTION = 0x2;
 
@@ -20,7 +19,6 @@ namespace EdiWare
         public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
         [System.Runtime.InteropServices.DllImport("user32.dll")]
         public static extern bool ReleaseCapture();
-
 
         public Form1()
         {
@@ -32,8 +30,9 @@ namespace EdiWare
 
         }
 
-        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        private void pnlWindow_MouseDown(object sender, MouseEventArgs e)
         {
+
             if (e.Button == MouseButtons.Left)
             {
                 ReleaseCapture();
@@ -41,9 +40,22 @@ namespace EdiWare
             }
         }
 
-        private void pictureBox1_Click(object sender, EventArgs e)
+        private void btnCrossMark_Click(object sender, EventArgs e)
         {
-            Environment.Exit(0);
+            Application.Exit();
+        }
+
+        private void btnIconStatus_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btnWindowControl_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Normal)
+                this.WindowState = FormWindowState.Maximized;
+            else
+                this.WindowState = FormWindowState.Normal;
         }
     }
 }
